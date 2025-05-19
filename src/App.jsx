19a38efbf9c978db82c368a5d8da3214f016f387
BaeJunH0 <bjh14757@naver.com>
2025-05-19 21:08:13 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+// 더 추가해야할 내용들 
+// import Guide from './pages/Guide';
+// import DataResult from './pages/DataResult';
+// Route path="/guide" element={<Guide />} />
+// <Route path="/DataResult" element={<DataResult />} />
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';   
@@ -6,8 +11,6 @@ import SignUp from './pages/SignUp';
 import FindID from './pages/FindID';
 import FindPassword from './pages/FindPassword';
 import MyPage from './pages/MyPage';
-import Guide from './pages/Guide';
-import DataResult from './pages/DataResult';
 
 function App() {
   return (
@@ -19,8 +22,6 @@ function App() {
         <Route path="/findid" element={<FindID />} />
         <Route path="/findpassword" element={<FindPassword />} />
         <Route path="/mypage" element={<MyPage />} />
-        <Route path="/guide" element={<Guide />} />
-        <Route path="/DataResult" element={<DataResult />} />
       </Routes>
     </Router>
   );
