2541fcaa6f6e03f5375e4de46583bd037370a913
이성진 <sungjin@iseongjin-ui-MacBookAir.local>
2025-05-29 17:38:24 +0900

    FileList page demo result box create

--- src/pages/FileList.jsx
+++ src/pages/FileList.jsx
@@ -3,6 +3,37 @@ import { useEffect, useState } from 'react';
 import '../styles/FileList.css';
 import Header from '../components/Header';
 import downloadIcon from '../assets/download.png';
+import resultIcon from '../assets/result.png';
+
+
+function getSeverity1(ahi) {
+  if (ahi < 5) return '[정상]';
+  if (ahi < 15) return '[주의]';
+  if (ahi < 30) return '[위험]';
+  return '[심각]';
+}
+
+function getSeverity2(ahi) {
+  if (ahi < 5) return '😊 걱정하지 마세요! 수면 상태가 아주 양호합니다. 😊';
+  if (ahi < 15) return '😐 수면 중 가벼운 문제가 관찰되었어요. 생활습관을 점검해보면 좋아요. 😐';
+  if (ahi < 30) return '😟 수면 질이 떨어지고 있어요. 개선이 필요할 수 있습니다. 😟';
+  return '🚨 수면 중 심각한 이상이 감지되었습니다. 전문가 상담이 꼭 필요해요. 🚨';
+}
+
+const getSeverityClass = (level) => {
+  switch (level) {
+    case '[정상]':
+      return 'severity-normal';
+    case '[주의]':
+      return 'severity-warning';
+    case '[위험]':
+      return 'severity-risk';
+    case '[심각]':
+      return 'severity-danger';
+    default:
+      return '';
+  }
+};
 
 function FileList() {
   const location = useLocation();
@@ -26,6 +57,11 @@ function FileList() {
     { name: 'ACC_4.csv' },
   ];
 
+  // 임의로 AHI값 생성
+  const [ahi] = useState(33);
+  const severity1 = getSeverity1(ahi);
+  const severity2 = getSeverity2(ahi);
+
   return (
     <>
       <Header />
@@ -33,6 +69,7 @@ function FileList() {
       <div className="visualize-container">
         <p>{date}에 측정된 수면 데이터 파일입니다.</p>
         <p>파일 이름을 클릭하면, 관련 시각화 자료를 볼 수 있습니다.</p>
+        <p>수면 데이터를 바탕으로 수면 무호흡 정도를 진단한 결과를 확인해보세요!</p>
 
         <div className="visualize-meta">
           <p>업로더: {user}</p>
@@ -77,11 +114,20 @@ function FileList() {
           </table>
         </div>
 
-        <div className="chart-wrapper">
-          <div>{/* chart 1 */}</div>
-          <div>{/* chart 2 */}</div>
-          <div>{/* chart 3 */}</div>
-          <div>{/* chart 4 */}</div>
+        <div className={`result-box ${getSeverityClass(severity1)}`}>
+          <img src={resultIcon} alt="진단 결과 아이콘" className="result-icon" />
+          <p className="ahi-title">
+            <strong>{user}</strong>님, <strong>{date}</strong>에 업로드된 데이터를 바탕으로 측정한 수면 무호흡증 진단 결과입니다.
+          </p>
+          <p className="ahi-result">
+            AHI 수치: <strong>{ahi}</strong><br></br>
+            Apnea Guard 진단 결과: <span className={getSeverityClass(severity1)}><strong>{severity1}</strong></span>
+          </p>
+          <hr />
+          <hr />
+          <p className="ahi-description">
+            {severity2}
+          </p>
         </div>
       </div>
     </>
