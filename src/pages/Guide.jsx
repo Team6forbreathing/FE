0542180141c16efc171e1eec775d75e9a90f2429
이성진 <sungjin@iseongjin-ui-MacBookAir.local>
2025-06-09 21:25:24 +0900

    guide modify

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -9,28 +9,29 @@ import step4Img from '../assets/step4.webp';
 import webstep1Img from '../assets/web_step1.png';
 import webstep2Img from '../assets/web_step2.png';
 import webstep3Img from '../assets/web_step3.png';
+import webstep4Img from '../assets/web_step4.png';
 
 function Guide() {
   const webguideSteps = [
     {
       id: 1,
-      text: 'data 페이지에서 날짜별로 수면 데이터를 확인할 수 있습니다.',
+      text: 'data 페이지에서 날짜별로 측정된 수면 데이터를 확인할 수 있습니다.',
       image: webstep1Img,
     },
     {
       id: 2,
-      text: '날짜를 클릭하여 범위를 설정하면 해당 날짜에 업로드된 수면 데이터 파일을 확인하고 다운받을 수 있습니다.',
+      text: '조회하고 싶은 날짜를 클릭하면, 업로드된 수면 데이터 파일을 확인하고 다운받을 수 있습니다.',
       image: webstep2Img,
     },
     {
       id: 3,
-      text: '데이터 파일을 클릭하면 PPGData.csv와 ACCData.csv 각각의 시각화된 자료를 확인할 수 있습니다.',
+      text: '데이터 파일을 클릭하면 파일의 시각화된 자료를 확인할 수 있습니다.',
       image: webstep3Img,
     },
     {
       id: 4,
-      text: '인가 허용자는 모든 사용자의 데이터를 볼 수 있고 업로드 가능합니다.',
-      image: step4Img,
+      text: '날짜별로 수면 무호흡 정도를 진단할 수 있습니다.',
+      image: webstep4Img,
     },
   ];
 
